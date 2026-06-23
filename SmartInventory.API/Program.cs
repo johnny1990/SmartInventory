ccb659ca1fc22bec485c051b7b5d89534063022b
Man Ioan <31920272+johnny1990@users.noreply.github.com>
2026-06-23 22:46:20 +0300

    Configure Swagger UI for API project

--- SmartInventory.API/Program.cs
+++ SmartInventory.API/Program.cs
@@ -9,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+});
+
 builder.Services.AddDbContext<SmartInventoryDbContext>(options =>
 {
     options.UseSqlServer(
@@ -17,10 +23,12 @@ builder.Services.AddDbContext<SmartInventoryDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// // Configure the HTTP request pipeline & Enable Swagger in development environment
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
